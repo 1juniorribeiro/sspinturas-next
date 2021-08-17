4e3cbf4e29dcede3922af71367c290b6c80568d0
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-08-17 15:24:28 -0400

    start of header

--- sspinturas/src/pages/_document.tsx
+++ sspinturas/src/pages/_document.tsx
@@ -1,36 +1,38 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class  extends Document {
-    render() {
-        return (   
-            <Html>
-            <Head>
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
 
-                {/* fonts */}
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@400;500;700&display=swap"
-                    rel="stylesheet"
-                />
+          {/* icons */}
+          <link 
+            rel="stylesheet"
+            href="../static/style.scss"
+          />
 
-                {/* icons */}
-                <link rel="stylesheet" href="../static/style.scss" />
+          {/* Swiper */}
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/swiper/swiper-bundle.min.css"
+          />
 
-                {/* Swiper */}
-                <link
-                    rel="stylesheet"
-                    href="https://unpkg.com/swiper/swiper-bundle.min.css"
-                />
-
-                {/* favicon */}
-                <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-            </Html>
-        )
-    }
+          {/* favicon */}
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- sspinturas/src/pages/index.tsx
+++ sspinturas/src/pages/index.tsx
@@ -1,37 +1,74 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
 
 import styles from '../styles/index.module.scss';
 
 export default function Home() {
-  return ( 
+  return (
     <>
-    <Head>
-      <title>SS Pinturas Mecanizadas</title>
-    </Head>
-    <body>
-    <header id="header" className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-      <nav>
-        <a className={styles.logo} href="#home"
-          ><img
-            src="/images/logo-horizontal.png"
-            alt="Logomarca Escrito SS Pinturas mecanizadas"
-        /></a>
-        <div>
-          <ul className={styles.grid}>
-            <li><a class="title" href="#home">Início</a></li>
-            <li><a class="title" href="#about">Sobre</a></li>
-            <li><a class="title" href="#services">Serviços</a></li>
-            <li><a class="title" href="#videos">Vídeos</a></li>
-            <li><a class="title" href="#contact">Contato</a></li>
-          </ul>
-        </div>
-        <div class="toggle icon-menu"></div>
-        <div class="toggle icon-close"></div>
-      </nav>
-      </div>
-    </header>
-    </body>
+      <Head>
+        <title>SS Pinturas Mecanizadas</title>
+      </Head>
+      <body>
+        <header id="header" className={styles.headerContainer}>
+          <div className={styles.headerContent}>
+            <nav>
+              <a className={styles.logo} href="#home"
+              ><Image
+                  src="/images/logo-horizontal.png"
+                  alt="Logomarca Escrito SS Pinturas mecanizadas"
+                  width={150}
+                  height={100}
+                  layout="responsive"
+                /></a>
+              <div className={styles.menu}>
+                <ul className={styles.grid}>
+                  <li><a className={styles.title} href="#home">Início</a></li>
+                  <li><a className={styles.title} href="#about">Sobre</a></li>
+                  <li><a className={styles.title} href="#services">Serviços</a></li>
+                  <li><a className={styles.title} href="#videos">Vídeos</a></li>
+                  <li><a className={styles.title} href="#contact">Contato</a></li>
+                </ul>
+              </div>
+              <div className={styles.toggle}></div> 
+              <div className={styles.toggle}></div>
+            </nav>
+          </div>
+        </header>
+
+        <main>
+
+        </main>
+      </body>
     </>
-   )
+  )
 }
+// icon-menu icon-close
+// export const getStaticProps: GetStaticProps = async () => {
+//   const nav = document.querySelector('#header nav')
+//   const toggle = document.querySelectorAll('nav .toggle')
+
+// for(const element of toggle) {
+//     element.addEventListener('click', function () {
+//       nav.classList.toggle('show')
+//     })
+//   }
+
+// // close the menu when click in the item
+
+// const links = document.querySelectorAll('nav ul li a')
+
+// for(const link of links) {
+//     link.addEventListener('click', function () {
+//       nav.classList.remove('show')
+//     })
+//   }
+
+//  return {
+//    props: {
+
+//    },
+//    revalidate: 60 * 60 * 24 * 30 //30 days
+//  }
+// }
